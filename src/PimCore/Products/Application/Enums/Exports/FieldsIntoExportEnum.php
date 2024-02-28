f524ec521908530715c32729b772b1917c23d49c
antonprokhorov <prohorov.a@iwis.io>
2024-02-28 11:24:22 +0200

    refactoring

---
+++ src/PimCore/Products/Application/Enums/Exports/FieldsIntoExportEnum.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\PimCore\Products\Application\Enums\Exports;
+
+enum FieldsIntoExportEnum: string
+{
+    case Measureelations = 'measureelations';      // Базова Од. вимірювання
+    case AlternativeUOM = 'alternativeuom';       // Альтернативна Од. вимірювання
+    case ManufacturerRelation = 'manufacturerelatoin';  // Виробник
+    case CountryRelations = 'countrurelations';     // Країна виробництва
+    case BrandRelations = 'brandrelations';       // Бренд
+    case CountryBrand = 'countrubrand';         // Країна бренду
+    case LineProductRelation = 'lineproductrelatoin';  // Лінія продукту
+    case RootCatRelations = 'rootcatrelations';     // Категорія (1 рівень)
+    case CategoriesRelations = 'categoriesrelations';  // Категорія (2 рівень)
+    case VariantRelations = 'variantrelations';     // Варіант (3 рівень)
+    case SubcategoryRelation = 'subcategoryrelation';  // Субкатегорія (3 рівень)
+}
+
